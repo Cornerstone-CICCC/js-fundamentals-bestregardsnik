3efe017b27f3eef3af4c5c4fc3f2455f0b60a0fc
bestregardsnik <bestregards.nik@gmail.com>
2026-01-30 20:47:47 -0800

    exercise 28 completed

--- exercises/ex28.js
+++ exercises/ex28.js
@@ -11,3 +11,23 @@ deleteMessage(index) - removes the message corresponding to the index provided
 addFriend() - increases the friend count by 1
 removeFriend() - decreases the friend count by 1
 */
+
+const facebookProfile = {
+    name: "John",
+    friends: 100,
+    messages: ["Hello!", "How are you?"],
+    postMessage: function(message) {
+        this.messages.push(message);
+    },
+    deleteMessage: function(index) {
+        this.messages.splice(index, 1);
+    },
+    addFriend: function() {
+        this.friends++;
+    },
+    removeFriend: function() {
+        this.friends--;
+    }
+};
+
+console.log(facebookProfile);
